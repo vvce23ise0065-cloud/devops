9857e9e01b7f5f5461327cf31b34b9c93f54a0e4
vvce23ise0051-creator <vvce23ise0051@vvce.ac.in>
2026-02-04 10:46:52 +0530

    Updated pgm.java

--- pgm.java
+++ pgm.java
@@ -1,5 +1,7 @@
 public class pgm {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+         System.out.println("Snehaaa Sureshhh");
+          System.out.println("Hello!");
     }
 }
